ea2299455beaf7cabf15cafe7b609e7867b38020
sahabuddin <sahabuddinriyaj984@gmail.com>
2020-09-19 16:05:08 +0600

    courses section complete in home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,18 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
-    dots:true,
+    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    dots:false,
     dotsEach:false,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
-            items:5
+            items:3
         }
     }
 })
